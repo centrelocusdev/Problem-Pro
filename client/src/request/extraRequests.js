340d6af69c2ed517d2fedef27f44e4560d679cc9
Centrelocus <127776664+centrelocusdev@users.noreply.github.com>
2024-04-05 15:36:58 +0530

    Update extraRequests.js

--- client/src/request/extraRequests.js
+++ client/src/request/extraRequests.js
@@ -1,6 +1,8 @@
 import axios  from "axios";
-const Backend_URL = process.env.REACT_APP_BACKEND_URL;
+//const Backend_URL = process.env.REACT_APP_BACKEND_URL;
 // const Backend_URL = "problem-pro-server-final-3zvw58361-centrelocusdev.vercel.app";
+const Backend_URL = "https://problem-pro.onrender.com";
+
 
 
 const contactUs = async (formData) => {
